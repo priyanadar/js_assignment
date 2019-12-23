ffb176385e58d80d6fd93357176177e592074113
Priya Nadar <priyanadar2000@gmail.com>
2019-12-23 15:49:49 +0530

    adding file

---
+++ addelements.js
@@ -0,0 +1,5 @@
+function sum(n) {
+    var a = eval(n.join('+'));
+    console.log(a);
+}
+sum([1, 2, 3, 4, 10]);
---
+++ sumofdigits.js
@@ -0,0 +1,18 @@
+function sumOfDigits(num){
+    var sum = 0;
+   
+    if(Number.isInteger(num) === false){
+      return sum;
+    }
+    
+    var str = num.toString();
+    for(i = 0; i <= str.length - 1; i++){
+      sum += +str[i];
+    }
+    
+    return sum;
+  }
+  console.log(sumOfDigits(414));
+
+
+  
